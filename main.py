fa1b13d6165d6b029cda5484e8a54a00ac6e6b6a
avanmet1 <55400228+avanmet1@users.noreply.github.com>
2019-11-12 12:09:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ line2_list = re.split(r'\n{1,}', edcomplete)
 line1s_item = random.choice(line1_list)
 line2s_item = random.choice(line2_list)
 line3s_item = random.choice(line1_list)
-line4s_item = random choice(line2_list)
+line4s_item = random.choice(line2_list)
 
 poem =  (line1s_item + "\r\n" + line2s_item + "\r\n" + line3s_item + "\r\n" + line4s_item)
 
