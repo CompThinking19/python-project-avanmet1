8a47dd52b50936d447b5149e7d3da9df449b5a5e
avanmet1 <55400228+avanmet1@users.noreply.github.com>
2019-11-12 11:29:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 line1_list = re.split(r'\n{1,}', leaves)
 
 line1s_item = random.choice(line1_list)
-line2s_itme = random.choice(line1_list)
+line2s_item = random.choice(line1_list)
 
 poem =  (line1s_item + "\r\n" + line2s_item)
 
