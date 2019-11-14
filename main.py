3817d087688a7e91b3e80c20ead71360e15fcde6
avanmet1 <55400228+avanmet1@users.noreply.github.com>
2019-11-14 12:53:39 -0500

    Changed regex
    Updated re.split to findall

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ edcomplete = source2.read()
 import re
 import random
 
-line1_list = re.split(r'\n{1}', leaves)
-line2_list = re.split(r'\n{1}', edcomplete)
+line1_list = re.findall(r'.+(?:\n)', leaves)
+line2_list = re.findall(r'.+(?:\n)', edcomplete)
 
 line1s_item = random.choice(line1_list)
 line2s_item = random.choice(line2_list)
